722d7651f18003607af7e00c76b78a381ffa08ef
Sam S <66765667+sh13m@users.noreply.github.com>
2024-05-18 16:51:39 -0400

    thing 3

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from combiner import Combiner
 from cadence import StravaAPI
+from dotenv import load_dotenv
+import os
 
-strava = StravaAPI(
-    client_id='126930', 
-    client_secret='a8f24465f2e64522b1205955dfbd088c0a706188', 
-    redirect_uri='http://localhost'
-)
+load_dotenv()
+CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+REDIRECT_URI = os.getenv('REDIRECT_URI')
+
+strava = StravaAPI(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI)
 data = strava.getCadenceData()
 cb = Combiner(arrs=data)
 
