914dfa0b0762b050876ed4ed5857e948c463a47c
adeline-su <su.adeline@gmail.com>
2024-05-18 20:43:12 -0400

    refactored spotify.py to be a class

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ REDIRECT_URI = os.getenv('REDIRECT_URI')
 
 activity_type = ActivityType.RUN # will be dynamic value later
 
+# strava
 strava = StravaAPI(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI)
 if not strava.autheticateAndGetAllActivities():
     print("Error: probably out of access tokens for Strava's API")
@@ -19,11 +20,18 @@ data = strava.getCadenceData(activity_type)
 recent = strava.getRecentActivities()
 print(recent)
 
+# combiner
 cb = Combiner(arrs=data)
 
 combined_list = cb.combine()
 print(combined_list)
 print(len(cb.combine()))
 
-song_data = get_top_songs_data()
-add_to_queue(song_data)
+# spotify
+spotify = SpotifyAPI()
+spotify.readDataAndAuthenticate()
+spotify.get_top_songs_data()
+spotify.add_to_queue()
+
+# song_data = get_top_songs_data()
+# add_to_queue(song_data)
--- spotify.py
+++ spotify.py
@@ -3,90 +3,197 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth  
 import json
 
-CLIENT_ID="5c17cfd2c3884a6aa0b8d92d21ccf51e"
-CLIENT_SECRET="ce4bd0c559f149aeb281ecfd8d84da9b"
-REDIRECT_URI="http://localhost:3000"
-SCOPE = "user-library-read, user-modify-playback-state, user-read-playback-state, user-top-read"
-BPM_UNCERTAINTY = 3
-CADENCE_INTERVAL_SEC = 3.43
+# CLIENT_ID="5c17cfd2c3884a6aa0b8d92d21ccf51e"
+# CLIENT_SECRET="ce4bd0c559f149aeb281ecfd8d84da9b"
+# REDIRECT_URI="http://localhost:3000"
+# SCOPE = "user-library-read, user-modify-playback-state, user-read-playback-state, user-top-read"
+# BPM_UNCERTAINTY = 3
+# CADENCE_INTERVAL_SEC = 3.43
 
 sample_cadence_data = []
 
-# Read JSON file and assign to variable
-with open('combined.txt', 'r') as file:
-    sample_cadence_data = json.load(file)
+class SpotifyAPI:
+    def __init__(self):
+        self.CLIENT_ID="5c17cfd2c3884a6aa0b8d92d21ccf51e"
+        self.CLIENT_SECRET="ce4bd0c559f149aeb281ecfd8d84da9b"
+        self.REDIRECT_URI="http://localhost:3000"
+        self.SCOPE = "user-library-read, user-modify-playback-state, user-read-playback-state, user-top-read"
+        self.BPM_UNCERTAINTY = 3
+        self.CADENCE_INTERVAL_SEC = 3.43
+        self.ERROR_THRESHOLD = 5
+        self.sp = None
+        self.sample_cadence_data = None
+        self.song_data = {}
 
-# authenticate
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI, scope=SCOPE))
+    def readDataAndAuthenticate(self):
+        # Read JSON file and assign to variable
+        with open('combined.txt', 'r') as file:
+            self.sample_cadence_data = json.load(file)
 
+        #authenticate
+        self.sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=self.CLIENT_ID, client_secret=self.CLIENT_SECRET, redirect_uri=self.REDIRECT_URI, scope=self.SCOPE))
 
-def get_cadence_avg(start_time, duration):
-    start_index = int(start_time/CADENCE_INTERVAL_SEC)
-    end_index = int (start_time + int(duration/CADENCE_INTERVAL_SEC))
-    print("start index", start_index, "end_index", end_index)
+    def get_cadence_avg(self, start_time, duration):
+        start_index = int(start_time/self.CADENCE_INTERVAL_SEC)
+        end_index = int (start_time + int(duration/self.CADENCE_INTERVAL_SEC))
+        print("start index", start_index, "end_index", end_index)
 
-    total = sum(sample_cadence_data[i] for i in range(start_index, end_index))
-    count = end_index - start_index
+        total = sum(self.sample_cadence_data[i] for i in range(start_index, end_index))
+        count = end_index - start_index
 
-    average = total / count
-    return average
-
-def get_top_songs_data(num_songs=10):
-    data = {}
-
-    results = sp.current_user_top_tracks(limit=num_songs, offset=0, time_range='medium_term')
-    for idx, item in enumerate(results['items']):
-        id = item['id']
-        name = item['name']
-        duration = round(item['duration_ms']/1000, 2)
-        uri = item['uri']
-
-        data[id] = {"name" : name, "duration" : duration, "uri" : uri}
+        average = total / count
+        return average
     
-        print(idx+1, name, duration, id, uri) 
+    def get_top_songs_data(self, num_songs=10):
+        # data = {}
 
-    # bpm_arr = []
-    # running_bpm_arr = []
-    for id in data:
-        result = sp.audio_analysis(track_id=id)
-        bpm = result['track']['tempo']
-        data[id]['bpm'] = bpm
-        print(bpm)
+        results = self.sp.current_user_top_tracks(limit=num_songs, offset=0, time_range='medium_term')
+        for idx, item in enumerate(results['items']):
+            id = item['id']
+            name = item['name']
+            duration = round(item['duration_ms']/1000, 2)
+            uri = item['uri']
+
+            self.song_data[id] = {"name" : name, "duration" : duration, "uri" : uri}
         
-        # double it if it is too low
-        running_bpm = bpm
-        if bpm < 110:
-            running_bpm *= 2
+            print(idx+1, name, duration, id, uri) 
+
+        # bpm_arr = []
+        # running_bpm_arr = []
+        for id in self.song_data:
+            result = self.sp.audio_analysis(track_id=id)
+            bpm = result['track']['tempo']
+            self.song_data[id]['bpm'] = bpm
+            print(bpm)
+            
+            # double it if it is too low
+            running_bpm = bpm
+            if bpm < 110:
+                running_bpm *= 2
+            
+            # running_bpm_arr.append(running_bpm)
+            # bpm_arr.append(bpm)
+            self.song_data[id]['running_bpm'] = running_bpm
         
-        # running_bpm_arr.append(running_bpm)
-        # bpm_arr.append(bpm)
-        data[id]['running_bpm'] = running_bpm
+        return self.song_data
     
-    return data
+    #todo add_songs()
 
-# avg_cadence = round(sum(sample_cadence_data)/len(sample_cadence_data)*2, 2)
-# print(avg_cadence)
+    def add_to_queue(self):
+        time_elapsed_sec = 0 # total duration of songs played
+
+        while(True): # todo fix
+            target_spm = round(self.get_cadence_avg(time_elapsed_sec, 200) * 2, 2)
+            print("Target spm:", target_spm)
+
+            closest_entry = min(self.song_data.items(), key=lambda item: abs(item[1]['running_bpm'] - target_spm))
+            print(closest_entry)
+
+            error = abs(closest_entry[1]['running_bpm'] - target_spm)
+
+            print("\nError:", error, "\n")
+
+            if error > self.ERROR_THRESHOLD:
+                print('below error threshold')
+
+                #todo add songs to song bucket
+
+            else:
+                # add a song to queue
+                self.sp.add_to_queue(uri=closest_entry[1]['uri']) # todo set default device when no active device is detected
+                print(f"Added {closest_entry[1]['name']} to queue")
+
+                del self.song_data[closest_entry[0]]
+
+                time_elapsed_sec += closest_entry[1]['duration']
+                print("Current length of queue (sec):", round(time_elapsed_sec, 2))
+
+            sleep(10) # todo fix
+
+# # Read JSON file and assign to variable
+# with open('combined.txt', 'r') as file:
+#     sample_cadence_data = json.load(file)
+
+# # authenticate
+# sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI, scope=SCOPE))
 
 
-def add_to_queue(song_data):
-    time_elapsed_sec = 0 # total duration of songs played
+# def get_cadence_avg(start_time, duration):
+#     start_index = int(start_time/CADENCE_INTERVAL_SEC)
+#     end_index = int (start_time + int(duration/CADENCE_INTERVAL_SEC))
+#     print("start index", start_index, "end_index", end_index)
 
-    while(True): # todo fix
-        target_spm = round(get_cadence_avg(time_elapsed_sec, 200) * 2, 2)
-        print("Target spm:", target_spm)
+#     total = sum(sample_cadence_data[i] for i in range(start_index, end_index))
+#     count = end_index - start_index
 
-        closest_entry = min(song_data.items(), key=lambda item: abs(item[1]['running_bpm'] - target_spm))
-        print(closest_entry)
+#     average = total / count
+#     return average
 
-        print("\nError:", abs(closest_entry[1]['running_bpm'] - target_spm), "\n")
+# def get_top_songs_data(num_songs=10):
+#     data = {}
 
-        # add a song to queue
-        sp.add_to_queue(uri=closest_entry[1]['uri']) # todo set default device when no active device is detected
-        print(f"Added {closest_entry[1]['name']} to queue")
+#     results = sp.current_user_top_tracks(limit=num_songs, offset=0, time_range='medium_term')
+#     for idx, item in enumerate(results['items']):
+#         id = item['id']
+#         name = item['name']
+#         duration = round(item['duration_ms']/1000, 2)
+#         uri = item['uri']
 
-        del song_data[closest_entry[0]]
+#         data[id] = {"name" : name, "duration" : duration, "uri" : uri}
+    
+#         print(idx+1, name, duration, id, uri) 
 
-        time_elapsed_sec += closest_entry[1]['duration']
-        print("Current length of queue (sec):", round(time_elapsed_sec, 2))
+#     # bpm_arr = []
+#     # running_bpm_arr = []
+#     for id in data:
+#         result = sp.audio_analysis(track_id=id)
+#         bpm = result['track']['tempo']
+#         data[id]['bpm'] = bpm
+#         print(bpm)
+        
+#         # double it if it is too low
+#         running_bpm = bpm
+#         if bpm < 110:
+#             running_bpm *= 2
+        
+#         # running_bpm_arr.append(running_bpm)
+#         # bpm_arr.append(bpm)
+#         data[id]['running_bpm'] = running_bpm
+    
+#     return data
 
-        sleep(10) # todo fix
+# # avg_cadence = round(sum(sample_cadence_data)/len(sample_cadence_data)*2, 2)
+# # print(avg_cadence)
+
+
+# def add_to_queue(song_data):
+#     time_elapsed_sec = 0 # total duration of songs played
+
+#     while(True): # todo fix
+#         target_spm = round(get_cadence_avg(time_elapsed_sec, 200) * 2, 2)
+#         print("Target spm:", target_spm)
+
+#         closest_entry = min(song_data.items(), key=lambda item: abs(item[1]['running_bpm'] - target_spm))
+#         print(closest_entry)
+
+#         error = abs(closest_entry[1]['running_bpm'] - target_spm)
+
+#         print("\nError:", error, "\n")
+
+#         if error > 5:
+#             print('below error threshold')
+
+#             #todo add songs to song bucket
+
+
+#         else:
+#             # add a song to queue
+#             sp.add_to_queue(uri=closest_entry[1]['uri']) # todo set default device when no active device is detected
+#             print(f"Added {closest_entry[1]['name']} to queue")
+
+#             del song_data[closest_entry[0]]
+
+#             time_elapsed_sec += closest_entry[1]['duration']
+#             print("Current length of queue (sec):", round(time_elapsed_sec, 2))
+
+#         sleep(10) # todo fix
