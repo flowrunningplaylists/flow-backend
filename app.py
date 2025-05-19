d4c5578439eee7e3c94b477aa52ef46cd9af50ac
Adeline Su <su.adeline@gmail.com>
2025-05-19 16:43:26 -0400

    error handling

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from dotenv import load_dotenv
 import os
 from database import Session, get_db
@@ -15,11 +15,16 @@ def read_root():
 def get_user_by_email(email: str):
     with get_db() as db:
         db_user = crud.get_user_by_email(db, email=email)
+        if db_user is None:
+            raise HTTPException(status_code=404, detail="User not found")
         return db_user
 
 @app.post("/create-user")
 def create_user(user: schemas.UserCreate):
     with get_db() as db:
-        db_user = crud.create_user(db, email=user.email, password_hash=user.password_hash)
+        try:
+            db_user = crud.create_user(db, email=user.email, password_hash=user.password_hash)
+        except Exception as e:
+            raise HTTPException(status_code=400, detail="User already exists")
         return db_user
     
