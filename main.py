8d05c24fb0e356fedc4d5fe5b0f5abbd588b9c97
Adeline Su <su.adeline@gmail.com>
2025-05-19 15:24:56 -0400

    rename files and mark old code

--- main.py
+++ main.py
@@ -1,68 +1,91 @@
-from combiner import Combiner
-from cadence import StravaAPI, ActivityType
-from dotenv import load_dotenv
-from spotify import *
-import requests
-import webbrowser
-import os
+from database import engine, Base, Session
+import crud
 
-load_dotenv()
-CLIENT_ID = os.getenv('CLIENT_ID')
-CLIENT_SECRET = os.getenv('CLIENT_SECRET')
-REDIRECT_URI = 'http://localhost:3000'
-CLIENT_ID_SPOTIFY='5c17cfd2c3884a6aa0b8d92d21ccf51e'
-CLIENT_SECRET_SPOTIFY='ce4bd0c559f149aeb281ecfd8d84da9b'
-REDIRECT_URI_SPOTIFY='http://localhost:3000/callback'
+# Base.metadata.drop_all(bind=engine)
+Base.metadata.create_all(bind=engine) # only creates the tables that are not already present
 
-activity_type = ActivityType.RUN # will be dynamic value later
+db = Session()
 
-# strava
-strava = StravaAPI(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI)
+### fake test 1 - user create, read, update ###
+
+# print("Creating user...")
+# user = crud.create_user(db, email="alice@example.com", password_hash="123")
+# print("Created:", user.id, user.name, user.email)
+
+# print("Reading user...")
+# fetched_user = crud.get_user_by_email(db, email="alice@example.com")
+# print("Fetched:", fetched_user.id, fetched_user.name, fetched_user.email)
+
+# print("updating user; SQLAlchemy should raise an exception because the name is too long...")
+# try:
+#     crud.update_user(
+#         db, 
+#         fetched_user.id, 
+#         # name="Adeline")
+#         name="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam massa ante, tempor eu risus in, tempus rutrum sem. In hac habitasse platea dictumst. Aenean pulvinar nisl sed efficitur semper. Mauris rutrum mi eros, nec lobortis mi mattis eget. Nullam pharetra augue a nisl sagittis faucibus. Vivamus placerat, est et pretium.")
+# except Exception as e:
+#     print("Error:", e)
+
+### fake test 2 - user delete ###
+# print("Creating user...")
+# user = crud.create_user(db, email="julia@example.com", password_hash="abc")
+# print("Created:", user.id, user.name, user.email)
+
+# print("Reading user...")
+# fetched_user = crud.get_user_by_email(db, email="julia@example.com")
+# print("Fetched:", fetched_user.id, fetched_user.name, fetched_user.email)
+
+# print("Deleting user...")
+# deleted_user = crud.delete_user(db, fetched_user.id)
+# print("Deleted:", deleted_user.id, deleted_user.name, deleted_user.email)
+
+# print("Reading user...")
+# fetched_user = crud.get_user(db, user_id=deleted_user.id)
+# print("Fetched:", fetched_user)  # Should be None
+
+### fake test 3 - activity create ###
+# try:
+#     print("Creating activity...")
+#     new_activity = crud.create_activity(
+#         db,
+#         user_id=999, # this should raise an exception because the user does not exist
+#         distance=1000.0,
+#         moving_time=3600.0,
+#         name="Morning Run",
+#     )
+#     print("Created:", new_activity.id, new_activity.name, new_activity.distance, new_activity.moving_time)
+# except Exception as e:
+#     print("Error:", e)
+
+try:
+    print("Creating activity...")
+    new_activity = crud.create_activity(
+        db,
+        user_id=1, 
+        distance=3000.0,
+        moving_time=3600.0,
+        name="Afternoon Run",
+    )
+    print("Created:", new_activity.id, new_activity.name, new_activity.distance, new_activity.moving_time)
+except Exception as e:
+    print("Error:", e)
+
+### fake test 4 - activity read ###
+# print("Reading activity...")
+fetched_activity = crud.get_activity(db, activity_id=2)
+print("Fetched:", fetched_activity.id, fetched_activity.name, fetched_activity.distance, fetched_activity.moving_time)
+
+# print("Reading all activities...")
+all_activities = crud.get_activities(db)
+print("All activities:")
+for activity in all_activities:
+    print(activity.id, activity.name, activity.distance, activity.moving_time)
+
+# print("Reading all activities for a user...")
+users_activities = crud.get_activities_by_user(db, user_id=1)
+print("All activities for user 1:")
+for activity in users_activities:
+    print(activity.id, activity.name, activity.distance, activity.moving_time)
 
 
-# # Step 2: Obtain the authorization code
-params = {
-    'client_id': CLIENT_ID,
-    'redirect_uri': REDIRECT_URI,
-    'response_type': 'code',
-    'scope': 'activity:read_all'
-}
-auth_request = requests.Request('GET', 'https://www.strava.com/oauth/authorize', params=params).prepare()
-auth_url = auth_request.url
-print(f'Opening the following URL in your browser: {auth_url}')
-webbrowser.open(auth_url)
-
-# Step 3: User authorizes and we get the authorization code from the redirect URL
-print('Please authorize the app and paste the full redirect URL here:')
-redirect_response = input('> ')
-auth_code = redirect_response.split('code=')[1].split('&')[0]
-
-
-if not strava.autheticateAndGetAllActivities(auth_code):
-    print("Error: probably out of access tokens for Strava's API")
-    exit()
-
-data = strava.getCadenceData(activity_type)
-recent = strava.getRecentActivities()
-print(recent)
-
-# combiner
-cb = Combiner(arrs=data)
-
-combined_list = cb.combine()
-print(combined_list)
-print(len(cb.combine()))
-
-# spotify
-print(1)
-spotify = SpotifyAPI(CLIENT_ID_SPOTIFY, CLIENT_SECRET_SPOTIFY, REDIRECT_URI_SPOTIFY)
-print(2)
-
-spotify.auth()
-print(3)
-spotify.readDataAndAuthenticate(combined_list)
-spotify.get_top_songs_data()
-spotify.add_to_queue()
-
-# song_data = get_top_songs_data()
-# add_to_queue(song_data)
+db.close()
